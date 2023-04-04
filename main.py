a684cb0386b5ea10ddbc285165270309e37234a7
Shehab222 <125226623+ShehabAgain@users.noreply.github.com>
2023-04-04 01:57:01 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,18 @@
+
+import random
+k=random.randint(10,17)
+p="i"*k
+a="H"+p
+if ((n:=len(a)) > 7):
+  print("max length exceeded")
+
+
+while True:
+  if ((n:=len(a))>1):
+    print(a)
+    a=a[:-1]
+  elif ((n:=len(a))>=1 and (n:=len(a))<=7):
+      for i in range(k):
+        a+="i"
+        print(a)
+    
